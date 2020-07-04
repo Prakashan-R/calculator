76f382c7bd45986ec6a9493066f5000e223a872c
Prakash R Vijay <prakashr714@gmail.com>
2020-07-04 15:40:05 +0530

    add a button in calculator window

--- calcu.py
+++ calcu.py
@@ -4,4 +4,11 @@ window.title('Calculator')
 window.geometry('350x350')
 e=Entry(window, width=42, borderwidth=5)
 e.place(x=0, y=0)
+
+
+
+b=Button(window, text='7', width=5)
+b.place(x=10, y=50)
+
+
 window.mainloop()
