c4df88ae105c7143384a5119e194e5218e0ec864
Prakash R Vijay <prakashr714@gmail.com>
2020-07-04 18:18:56 +0530

    modified old calculater to new type calculator , multiple operator works at a time

--- calcu.py
+++ calcu.py
@@ -11,49 +11,12 @@ def btclick(num):
     e.delete(0, END)
     e.insert(0,str(current)+str(num))
 
-def div():
-    firstno = e.get()
-    global operant
-    operant = 'divition'
-    global f
-    f = int(firstno)
-    e.delete(0, END)
-
-def mul():
-    firstno = e.get()
-    global operant
-    operant = 'multiplication'
-    global f
-    f = int(firstno)
-    e.delete(0, END)
-
-def sub():
-    firstno = e.get()
-    global operant
-    operant = 'subtraction'
-    global f
-    f = int(firstno)
-    e.delete(0, END)
-
-def add():
-    firstno = e.get()
-    global operant
-    operant = 'addition'
-    global f
-    f = int(firstno)
-    e.delete(0, END)
 
 def eql():
-    secondno=e.get()
+    firstno = e.get()
     e.delete(0, END)
-    if operant=='addition':
-        e.insert(0, f+int(secondno))
-    elif operant=='subtraction':
-        e.insert(0, f-int(secondno))
-    elif operant=='multiplication':
-        e.insert(0, f*int(secondno))
-    elif operant=='divition':
-        e.insert(0, f/int(secondno))
+    result=eval(firstno)
+    e.insert(0, result)
 
 def clear():
     e.delete(0, END)
@@ -68,7 +31,7 @@ b.place(x=80, y=50)
 b=Button(window, text='9', width=5, command=lambda:btclick(9))
 b.place(x=150, y=50)
 
-b=Button(window, text='/', width=5, command=div)
+b=Button(window, text='/', width=5, command=lambda:btclick('/'))
 b.place(x=220, y=50)
 
 b=Button(window, text='4', width=5, command=lambda:btclick(4))
@@ -80,7 +43,7 @@ b.place(x=80, y=100)
 b=Button(window, text='6', width=5, command=lambda:btclick(6))
 b.place(x=150, y=100)
 
-b=Button(window, text='*', width=5, command=mul)
+b=Button(window, text='*', width=5, command=lambda:btclick('*'))
 b.place(x=220, y=100)
 
 b=Button(window, text='1', width=5, command=lambda:btclick(1))
@@ -92,7 +55,7 @@ b.place(x=80, y=150)
 b=Button(window, text='3', width=5, command=lambda:btclick(3))
 b.place(x=150, y=150)
 
-b=Button(window, text='-', width=5, command=sub)
+b=Button(window, text='-', width=5, command=lambda:btclick('-'))
 b.place(x=220, y=150)
 
 b=Button(window, text='.', width=5, command=lambda:btclick('.'))
@@ -104,7 +67,7 @@ b.place(x=80, y=200)
 b=Button(window, text='=', width=5, command=eql)
 b.place(x=150, y=200)
 
-b=Button(window, text='+', width=5, command=add)
+b=Button(window, text='+', width=5, command=lambda:btclick('+'))
 b.place(x=220, y=200)
 
 b=Button(window, text='Clear', width=5, command=clear)
